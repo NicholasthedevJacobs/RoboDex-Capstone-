a3de35ecca9ceadbb4c3741054e9f3862ff2edef
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-10-13 12:42:38 -0500

    Created an interfact for each model in the contracts folder.  These inherit from the IRepositoryBase of each model.

---
+++ RoboDex (Capstone)/Contracts/IInboxRepository.cs
@@ -0,0 +1,12 @@
+﻿using RoboDex__Capstone_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboDex__Capstone_.Contracts
+{
+    public interface IInboxRepository : IRepositoryBase<Inbox> 
+    {
+    }
+}
