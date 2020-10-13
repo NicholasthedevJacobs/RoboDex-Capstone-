3a96d8ebd705f804444341cf826dd488d68f15ba
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-10-13 15:27:23 -0500

    Created the RoboDexerController, and filled logic for the indexand create methods.  Now sucessfully adds the robodexer's info to the database.

--- RoboDex (Capstone)/Controllers/RoboDexerController.cs
+++ RoboDex (Capstone)/Controllers/RoboDexerController.cs
@@ -1,46 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RoboDex__Capstone_.Contracts;
+using RoboDex__Capstone_.Models;
 
 namespace RoboDex__Capstone_.Controllers
 {
-    public class RoboDexController : Controller
+    public class RoboDexerController : Controller
     {
         private IRepositoryWrapper _repo;
 
 
-        public RoboDexController(IRepositoryWrapper repo)
+        public RoboDexerController(IRepositoryWrapper repo)
         {
             _repo = repo;
 
         }
-        // GET: RoboDexController
+        // GET: RoboDexerController
         public ActionResult Index()
         {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var roboDexerUser = _repo.RoboDexer.FindByCondition(r => r.IdentityUserId == userId).SingleOrDefault();
+            if (roboDexerUser == null)
+            {
+                return RedirectToAction("Create");
+            }
             return View();
         }
 
-        // GET: RoboDexController/Details/5
+        // GET: RoboDexerController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
 
-        // GET: RoboDexController/Create
+        // GET: RoboDexerController/Create
         public ActionResult Create()
         {
-            return View();
+            RoboDexer roboDexer = new RoboDexer();
+            return View(roboDexer);
         }
 
-        // POST: RoboDexController/Create
+        // POST: RoboDexerController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(RoboDexer robodexer)
         {
+            if (ModelState.IsValid)
+            {
+                var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                robodexer.IdentityUserId = userId;
+
+                _repo.RoboDexer.Create(robodexer);
+                _repo.Save();
+            }
+            
+
+
             try
             {
                 return RedirectToAction(nameof(Index));
@@ -51,13 +71,13 @@ namespace RoboDex__Capstone_.Controllers
             }
         }
 
-        // GET: RoboDexController/Edit/5
+        // GET: RoboDexerController/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: RoboDexController/Edit/5
+        // POST: RoboDexerController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -72,13 +92,13 @@ namespace RoboDex__Capstone_.Controllers
             }
         }
 
-        // GET: RoboDexController/Delete/5
+        // GET: RoboDexerController/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: RoboDexController/Delete/5
+        // POST: RoboDexerController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
