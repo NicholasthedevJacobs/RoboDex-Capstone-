fa93369672a1632420276ebf29f3d3eb6b04f64c
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-10-13 09:56:51 -0500

    Created model for location, and added properties.

---
+++ RoboDex (Capstone)/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboDex__Capstone_.Models
+{
+    public class Location
+    {
+        public int LocationId { get; set; }
+        public string MainLocation { get; set; }
+        public string? SecondaryLocation { get; set; }
+
+    }
+}
