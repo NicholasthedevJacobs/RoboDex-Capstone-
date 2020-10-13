c52f09c5a239e84cb750d88e53f7d4bc8527634e
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-10-13 08:38:40 -0500

    Innit.

---
+++ RoboDex (Capstone)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoboDex__Capstone_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
