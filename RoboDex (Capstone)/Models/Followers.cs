bdae878ecaffda21210de02947fac893250b1191
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-10-13 09:54:00 -0500

    Created model for followers, and added properties.

---
+++ RoboDex (Capstone)/Models/Followers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboDex__Capstone_.Models
+{
+    public class Followers
+    {
+        public int FollowersId { get; set; }
+        public int SellerId { get; set; }
+        public int BuyerId { get; set; }
+
+    }
+}
