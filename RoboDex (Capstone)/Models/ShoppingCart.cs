fb67377ca260ead7412dbeb919dc139c5bb380df
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-10-13 09:55:14 -0500

    Created a model for the SHoppingCart, and added properties.

---
+++ RoboDex (Capstone)/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoboDex__Capstone_.Models
+{
+    public class ShoppingCart
+    {
+        public int ShoppingCartId { get; set; }
+        public int ItemId { get; set; }
+
+    }
+}
